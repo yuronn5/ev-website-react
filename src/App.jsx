ada2252bca8a4e2223526e4a9864a2b5a898a400
yurii bilous <yurii.b@elogic.com.ua>
2024-06-02 18:38:58 +0300

    background component styling and adding content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import Background from "./components/background/Background";
+import { useState } from "react";
 
 
 const App = () => {
+  let heroData = [
+    {
+      text1: 'Dive into',
+      text2: 'what you love'
+    },
+    {
+      text1: 'Indulge',
+      text2: 'your passions'
+    },
+    {
+      text1: 'Give in to',
+      text2: 'your passions'
+    }
+  ]
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
   return (
-    <div>App</div>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+    </div>
   )
 }
 
--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,9 +1,28 @@
-import './Background.css'
+import './Background.css';
+import video1 from '../../assets/video1.mp4';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
 
-const Background = () => {
-  return (
-    <div>Background</div>
-  )
+const Background = ({playStatus, heroCount}) => {
+  
+    if (playStatus) {
+      return (
+        <video className='background' autoPlay loop muted>
+            <source src={video1} type="video/mp4"/>
+        </video>
+      )
+    }
+    else if (heroCount === 0) {
+        return <img className='background' src={image1} alt=''></img>
+    }
+    else if (heroCount === 1) {
+        return <img className='background' src={image2} alt=''></img>
+    }
+    else if (heroCount === 2) {
+        return <img className='background' src={image3} alt=''></img>
+    }
+
 }
 
 export default Background
